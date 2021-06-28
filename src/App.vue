8ab8ca2a502cfbaf863bd18e72ac6a1e4077037a
stas <stanislav.tutov@norbit.ru>
2021-06-28 22:55:23 +0300

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
 
+  <div id="app">
+
+    <button
+        v-if="catalog"
+        @click="reloadPage"
+    >Сбросить значения
+    </button>
+
+    <button
+        v-if="showTree"
+        @click="hide"
+    >Скрыть JSON
+    </button>
+    <button
+        v-if="!showTree"
+        @click="show"
+    >Показать
+    </button>
+
+    <TreeBrowser
+        :node="catalog"
+        class="check"
+    />
+
+    <div v-if="showTree">
+      <pre> {{ catalog }}</pre>
+    </div>
+
+  </div>
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TreeBrowser from "./components/TreeBrowser.vue";
+import catalog from "./catalog.json";
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      catalog,
+      showTree: [],
+    }
+  },
+  methods: {
+    show: function () {
+      this.showTree = true
+    },
+    hide: function () {
+      this.showTree = false
+    },
+    reloadPage() {
+      window.location.reload();
+    },
+
+  },
+
   components: {
-    HelloWorld
+    TreeBrowser,
   }
 }
 </script>
 
 <style>
+button {
+  margin-bottom: 34px;
+}
+
+.check {
+  padding: 100px;
+}
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  margin-left: 311px
 }
 </style>
